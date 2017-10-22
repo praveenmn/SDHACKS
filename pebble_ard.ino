4d7062a4f721784c85ee77ce4d6cfda3b54056fc
Praveen.m.n <praveen.m.n@hotmail.com>
2017-10-22 09:42:27 -0700

    Add files via upload

---
+++ pebble_ard.ino
@@ -0,0 +1,46 @@
+#include <CurieBLE.h>
+
+BLEPeripheral blePeripheral;       // BLE Peripheral Device (the board you're programming)
+BLEService heartRateService("180D"); // BLE Heart Rate Service
+
+// BLE Heart Rate Measurement Characteristic"
+BLECharacteristic heartRateChar("2A37",  // standard 16-bit characteristic UUID
+    BLERead | BLENotify, 2);  // remote clients will be able to get notifications if this characteristic changes
+                              // the characteristic is 2 bytes long as the first field needs to be "Flags" as per BLE specifications
+                              // https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
+
+int oldHeartRate = 0;  // last heart rate reading from analog input
+long previousMillis = 0;  // last time the heart rate was checked, in ms
+
+void setup() {
+  Serial.begin(9600);    // initialize serial communication
+  pinMode(13, OUTPUT);   // initialize the LED on pin 13 to indicate when a central is connected
+
+  /* Set a local name for the BLE device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this BLE device
+     The name can be changed but maybe be truncated based on space left in advertisement packet */
+  blePeripheral.setLocalName("Emergency");
+  blePeripheral.setAdvertisedServiceUuid(heartRateService.uuid());  // add the service UUID
+  blePeripheral.addAttribute(heartRateService);   // Add the BLE Heart Rate service
+  blePeripheral.addAttribute(heartRateChar); // add the Heart Rate Measurement characteristic
+
+  /* Now activate the BLE device.  It will start continuously transmitting BLE
+     advertising packets and will be visible to remote BLE central devices
+     until it receives a new connection */
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  if(voltage == 5.0){
+      Serial.println(voltage);
+      blePeripheral.begin();
+  }
+  blePeripheral.disconnect();
+}
+
